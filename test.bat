2957cd07a423763979f76cbd829f3b38222c6db1
Choo <yan.pheng.choo@intel.com>
2018-12-04 12:11:14 +0800

    removed test.bat

--- test.bat
+++
@@ -1,5 +0,0 @@
-@echo OFF
-CALL C:\Intel\computer_vision_sdk\bin\setupvars.bat
-cd build
-human_pose_estimation_demo -m C:\Intel\computer_vision_sdk\deployment_tools\intel_models\human-pose-estimation-0001\FP32\human-pose-estimation-0001.xml
-pause
